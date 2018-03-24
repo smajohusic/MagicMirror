1488df44acf757a0e690bd7d271b5cd49be39d92
Smajo Husic <smajo@sempro.no>
2018-03-24 21:12:36 +0100

    no message

--- python/motion/pir.py
+++ python/motion/pir.py
@@ -8,11 +8,12 @@ GPIO.setup(3, GPIO.OUT)         #LED output pin
 
 while True:
     i=GPIO.input(11)
+    
     if i==0:                 #When output from motion sensor is LOW
-    print("No intruders");
-    GPIO.output(3, 0)  #Turn OFF LED
-    time.sleep(0.1)
+        print("No intruders")
+        GPIO.output(3, 0)  #Turn OFF LED
+        time.sleep(0.1)
     elif i==1:               #When output from motion sensor is HIGH
-    print("Intruder detected")
-    GPIO.output(3, 1)  #Turn ON LED
-    time.sleep(0.1)
+        print("Intruder detected")
+        GPIO.output(3, 1)  #Turn ON LED
+        time.sleep(0.1)
