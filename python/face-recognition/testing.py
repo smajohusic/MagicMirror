ea403a0c980fd533356a42577945a7f101fc97ce
Smajo Husic <smajo@sempro.no>
2018-03-25 21:25:42 +0200

    no message

--- python/face-recognition/testing.py
+++ python/face-recognition/testing.py
@@ -2,6 +2,12 @@ import os
 import configparser
 import RPi.GPIO as GPIO
 
+# Initilize and set the GPIO
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11, GPIO.IN) #Read output from PIR motion sensor
+GPIO.setup(3, GPIO.OUT) #LED output pin
+
 def detectMotion():
     detect = True
 
