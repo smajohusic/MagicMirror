7bdee11fdbad3ae23560236659475639fa13a490
Smajo Husic <smajo@sempro.no>
2018-03-25 17:54:51 +0200

    no message

---
+++ python/face-recognition/images.py
@@ -0,0 +1,5 @@
+import os
+
+currentPath = os.path.dirname(__file__) # Absolute dir the script is in
+filepath = "../images/" # The path where the pictures are uploaded
+fileList = os.listdir(os.path.join(currentPath, filepath))
