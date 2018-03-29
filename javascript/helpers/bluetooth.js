417174026bad5357d56df9c1953cee020030011e
Smajo Husic <smajo@sempro.no>
2018-03-29 11:51:20 +0200

    Work work wokr

--- javascript/helpers/bluetooth.js
+++ javascript/helpers/bluetooth.js
@@ -1,4 +1,5 @@
 var noble = require('noble');
+var async = require('async');
 
 // This needs to be refactored to the config but for some reason no devices are fund when defining the array
 var deviceUUIDs = ['e00e9eb160ef4ade820589526365ed7b'];
@@ -37,12 +38,76 @@ function explore(peripheral) {
 
     deviceUUIDs.forEach(function (deviceUUID) {
       if (deviceUUID === peripheral.uuid) {
-        peripheral.discoverAllServicesAndCharacteristics(function(error, services, characteristics) {
-          // console.log('discoverAllServicesAndCharacteristics', error);
-          // console.log(services);
-          // console.log(characteristics);
+        /*
+        * Discover Services
+        */
+        peripheral.discoverServices([], function (error, services) {
+          if (!services.length) {
+            peripheral.disconnect();
+          }
 
-          
+          services.forEach(function (service) {
+            // Is this only for iphone?
+            service.discoverCharacteristics(['2a19'], function(error, characteristics) {
+              // Get the battery service characteristic and read the value
+              characteristics.forEach(function (characteristic) {
+                if (characteristic.uuid === '2a19' && characteristic._serviceUuid === '180f') {
+                  characteristic.read(function (error, data) {
+                    console.log('battery level is now: ', data.readUInt8(0) + '%');
+                  });
+
+                  // To enable notify
+                  characteristic.subscribe(function(error) {
+                    console.log('battery level notification on');
+                  });
+                }
+              });
+            });
+
+
+            /*
+            * Discover Characteristics
+            */
+            // service.discoverCharacteristics([], function(error, characteristics) {
+              // characteristics.forEach(function (characteristic) {
+              //
+              //
+              //   // if (characteristic.uuid === '2a19') {
+              //     characteristic.read(function (error, data) {
+              //       if (data) {
+              //         var string = data.toString('ascii');
+              //
+              //         console.log(data);
+              //         console.log('utf-8: ' + data.toString('utf8'));
+              //         console.log('ascii: ' + data.toString('ascii'));
+              //
+              //         var characteristicInfo = '\n    value       ' + data.toString('hex') + ' | \'' + string + '\'';
+              //       }
+              //
+              //       // console.log(characteristicInfo);
+              //     });
+              //
+              //     characteristic.discoverDescriptors(function(error, descriptors) {
+              //       descriptors.forEach(function (descriptor) {
+              //         descriptor.readValue(function (error, data) {
+              //           if (data) {
+              //             var characteristicInfo = ' (' + data.toString() + ')';
+              //
+              //             // console.log(characteristicInfo);
+              //           }
+              //         });
+              //       });
+              //     });
+              //   // }
+              //
+              //   // characteristic.on('data', function (data) {
+              //   //   console.log(data);
+              //   // });
+              //
+              //
+              // });
+            // });
+          });
         });
       }
     });
